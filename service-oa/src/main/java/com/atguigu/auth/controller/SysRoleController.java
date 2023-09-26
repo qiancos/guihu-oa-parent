323f7a822a0933f7a37de498a63ad4a1cbc2d24f
GAOXU <3285398657@qq.com>
2023-09-26 17:49:46 +0800

    dd

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -2,7 +2,9 @@ package com.atguigu.auth.controller;
 
 
 import com.atguigu.auth.service.SysRoleService;
+import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +16,28 @@ import java.util.List;
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
 
+
+    //http://localhost:8800/admin/system/sysRole
+
     //注入service
     @Autowired
     private SysRoleService sysRoleService;
 
 
     //查询所有
+    @ApiOperation("")
     @GetMapping("/findAll")
-    public List<SysRole> findAll(){
+    public Result<List<SysRole>> findAll(){
 
         List<SysRole> roleList = sysRoleService.list();
-        return roleList;
+        return Result.ok(roleList);
     }
 
+    //统一返回数据结果
+    @GetMapping("/findAll2")
+    public Result findAll2(){
+
+        List<SysRole> roleList = sysRoleService.list();
+        return Result.ok(roleList);
+    }
 }
